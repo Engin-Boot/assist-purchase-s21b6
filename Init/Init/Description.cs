d2bdb5bc89da4f73e0b2bbcce02d955c7e170aa7
venkatesh-cs <venkatesh_cs@outlook.com>
2020-10-16 09:42:44 +0530

    Added Excel Reader

---
+++ Init/Init/Description.cs
@@ -0,0 +1,29 @@
+﻿namespace DataFeeder
+{
+    public class Dimensions
+    {
+        public double Height;
+        public double Width;
+        public double Length;
+    }
+    public class Description
+    {
+        public string ProductId { get; set; }
+        public string ModelNumber { get; set; }
+        public string ProductName { get; set; }
+
+        //In cm (Validation required
+        public Dimensions Dimensions = new Dimensions();
+        //In inches (Validation required 
+        public double ScreenSize { get; set; }
+        public double Weight { get; set; }
+        public bool HasHandle { get; set; }
+        public bool IsTouchScreen { get; set; }
+        public bool IsCeCertified { get; set; }
+
+        //Can be either LCD or LED, needs validation
+        public string ScreenType { get; set; }
+        public bool HasBattery { get; set; }
+
+    }
+}
---
+++ Init/Init/Init.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace Init
+{
+    public class ExcelReader
+    {
+        private readonly bool _hasInit;
+        public ExcelReader(string fileName)
+        {
+            _excelApplication = new Application();
+            _wb = _excelApplication.Workbooks.Open(fileName);
+            _hasInit = true;
+        }
+        public string ReadCell(int i, int j)
+        {
+            string cellData;
+            cellData = _ws.Cells[i, j].Value.ToString();
+            if (cellData == null || cellData == "")
+            {
+                return "";
+            }
+            else
+            {
+                return cellData;
+            }
+        }
+        public void CloseWorkBook()
+        {
+            _wb.Close();
+            _excelApplication.Quit();
+        }
+
+        public int GetNumberOfRowsInSheet()
+        {
+            var range = _ws.UsedRange;
+            return range.Rows.Count;
+        }
+
+        public int GetNumberOfColumnsInSheet()
+        {
+            var range = _ws.UsedRange;
+            return range.Columns.Count;
+        }
+
+        public void SetExcelSheetNumber(int sheetNumber)
+        {
+            // ReSharper disable once RedundantBoolCompare
+            if (_hasInit == true)
+            {
+                _ws = _wb.Worksheets[sheetNumber];
+            }
+        }
+
+        private readonly _Application _excelApplication;
+        private readonly Workbook _wb;
+        private Worksheet _ws;
+    }
+}
