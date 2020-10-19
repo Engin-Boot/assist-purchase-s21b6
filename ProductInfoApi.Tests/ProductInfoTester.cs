fa90a0f315a01d58566b58f53735b59f36f08c9e
SoniaVadhnani <SoniaVadhnani@users.noreply.github.com>
2020-10-19 18:29:49 +0530

    Tests Named Correctly

--- ProductInfoApi.Tests/ProductInfoTester.cs
+++ ProductInfoApi.Tests/ProductInfoTester.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -100,17 +99,22 @@ namespace ProductInfoApi.Tests
                 }
             }
             );
-            // return dictOfProducts;
         }
 
+        #region DictionaryInitializationTests
+
         [Fact]
-        public void Test1()
+        public void WhenObjectOfCharacteristicWiseFilterIsCreatedItInitialisesDictoinaryOfProducts()
         {
             Assert.True(_filter.DictionaryOfProducts.Count == 4);
         }
 
+        #endregion
+
+        #region AllProductsRetrievalTests
+
         [Fact]
-        public void Test2()
+        public void WhenGetAllProductsThenReturnDictionaryOfProducts()
         {
             Assert.True(_filter.GetAllProducts().Count > 0);
             Assert.True(_filter.GetAllProducts().Count == 4);
@@ -126,8 +130,12 @@ namespace ProductInfoApi.Tests
             Assert.Equal("abc1", _filter.GetAllProductIds().ElementAt(0));
         }
 
+        #endregion
+
+        #region TouchScreenFilterTests
+
         [Fact]
-        public void Touchscreen()
+        public void WhenTouchscreenFeatureIsRequiredByClientThenReturnThoseProductsIdsWithIsTouchscreenTrue()
         {
             var checkTouchScreenProducts = new List<string>() { "abc1", "abc2" };
             Assert.Equal(new List<string>() { "abc1" },
@@ -135,129 +143,141 @@ namespace ProductInfoApi.Tests
         }
 
         [Fact]
-        public void Touchscreen1()
+        public void WhenTouchscreenFeatureIsRequiredByClientThenReturnNothingIfProductsIdsWithIsTouchscreenTrueNotThere()
         {
             var checkTouchScreenProducts = new List<string>() { "abc2", "abc3" };
             Assert.Empty(_filter.FilterByTouchscreen(checkTouchScreenProducts, "true"));
         }
 
         [Fact]
-        public void Touchscreen2()
+        public void WhenEmptyInputListOfProductIdsIsPassedToTouchscreenFilterThenSendBackBadRequestStatusCode()
         {
             var checkTouchScreenProducts = new List<string>() { "" };
             var output = _filter.FilterByTouchscreen(checkTouchScreenProducts, "true");
             Assert.Equal(HttpStatusCode.BadRequest, output.ElementAt(0));
         }
         [Fact]
-        public void Touchscreen3()
+        public void WhenInvalidInputResourceIsPassedToTouchscreenFilterThenSendBackBadRequestStatusCode()
         {
             var checkTouchScreenProducts = new List<string>() { "abc1" };
             var output = _filter.FilterByTouchscreen(checkTouchScreenProducts, "rue");
             Assert.Equal(HttpStatusCode.BadRequest, output.ElementAt(0));
         }
 
+        #endregion
+
+        #region HandleFilterTests
+
         [Fact]
-        public void Handle()
+        public void WhenHandleIsRequiredByClientThenReturnProductIdsFromPassedListThatContainsHandle()
         {
             var checkHandleProducts = new List<string>() { "abc1", "abc2" };
             Assert.Equal(new List<string>() { "abc1", "abc2" },
                 _filter.FilterByHandle(checkHandleProducts, "true"));
         }
         [Fact]
-        public void Handle1()
+        public void WhenHandleFeatureIsRequiredByClientThenReturnNothingIfPassedProductsHaveNoHandle()
         {
             var checkHandleProducts = new List<string>() { "abc3", "abc4" };
-            Assert.False(_filter.FilterByHandle(checkHandleProducts, "true").Any());
+            Assert.Empty(_filter.FilterByHandle(checkHandleProducts, "true"));
         }
 
         [Fact]
-        public void Handle2CheckingBool()
+        public void HandleCheckingBool()
         {
             var checkHandleProducts = new List<string>() { "abc3" };
             var output = _filter.FilterByHandle(checkHandleProducts, "rue");
             Assert.Equal(HttpStatusCode.BadRequest, output.ElementAt(0));
         }
         [Fact]
-        public void HandleCheckingNull()
+        public void WhenEmptyInputListOfProductIdsIsPassedToHandleFilterThenSendBackBadRequestStatusCode()
         {
             var checkHandleProducts = new List<string>() { "" };
             var output = _filter.FilterByHandle(checkHandleProducts, "true");
             Assert.Equal(HttpStatusCode.BadRequest, output.ElementAt(0));
         }
+
+        #endregion
+
+        #region CeCertificationFilterTests
+
         [Fact]
-        public void CeCertification()
+        public void WhenCeCertificationFeatureIsRequiredByClientThenReturnProductsHavingCeCertification()
         {
             var checkCeProducts = new List<string>() { "abc1", "abc2" };
             Assert.Equal(new List<string>() { "abc1", "abc2" },
                 _filter.FilterByCeCertification(checkCeProducts, "true"));
         }
         [Fact]
-        public void CeCertification1()
+        public void WhenCeCertificationFeatureIsRequiredByClientThenReturnNothingIfPassedProductsHaveNoCeCertification()
         {
             var checkCeProducts = new List<string>() { "abc3", "abc4" };
-            Assert.False(_filter.FilterByCeCertification(checkCeProducts, "true").Any());
+            Assert.Empty(_filter.FilterByCeCertification(checkCeProducts, "true"));
         }
         [Fact]
-        public void CeCertification21()
+        public void CeCertificationBoolCheck()
         {
             var checkCeProducts = new List<string>() { "abc3", "abc4" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByCeCertification(checkCeProducts, "tru").ElementAt(0));
         }
-        //[Fact]
-        //public void CeCertification2()
-        //{
-        //    var checkCeProducts = new List<string>() { "" };
-        //    Assert.True(_filter.FilterByCeCertification(checkCeProducts, "true").Any());
-        //}
-        //[Fact]
-        //public void CeCertification4()
-        //{
-        //    var checkCeProducts = new List<string>();
-        //    Assert.True(_filter.FilterByCeCertification(checkCeProducts, "true").Any());
-        //}
+        [Fact]
+        public void WhenEmptyInputListOfProductIdsIsPassedCeCertificationFilterThenSendBackBadRequestStatusCode()
+        {
+            var checkCeProducts = new List<string>() { "" };
+            Assert.Equal(HttpStatusCode.BadRequest,_filter.FilterByCeCertification(checkCeProducts, "true").ElementAt(0));
+        }
 
+
+        #endregion
+
+        #region BatteryFilterTests
 
         [Fact]
-        public void Battery()
+        public void WhenBatteryFeatureIsRequiredByClientThenReturnProductsThatHaveBattery()
         {
             var checkBatteryProducts = new List<string>() { "abc1", "abc2", "abc3" };
             Assert.Equal(new List<string>() { "abc1", "abc2" },
                 _filter.FilterByBatteryAvailability(checkBatteryProducts, "true"));
         }
         [Fact]
-        public void Battery1()
+        public void WhenBatteryFeatureIsRequiredByClientThenReturnNothingIfPassedProductsHaveNoBattery()
         {
             var checkBatteryProducts = new List<string>() { "abc3", "abc4" };
-            Assert.False(_filter.FilterByBatteryAvailability(checkBatteryProducts, "true").Any());
+            Assert.Empty(_filter.FilterByBatteryAvailability(checkBatteryProducts, "true"));
         }
         [Fact]
-        public void BatteryBool()
+        public void BatteryBoolCheck()
         {
             var checkBattery = new List<string>() { "abc1" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByBatteryAvailability(checkBattery, "tru").ElementAt(0));
         }
         [Fact]
-        public void BatteryNull()
+        public void WhenEmptyInputListOfProductIdsIsPassedToBatteryFilterThenSendBackBadRequestStatusCode()
         {
             var checkBattery = new List<string>() { "" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByBatteryAvailability(checkBattery, "true").ElementAt(0));
         }
 
+
+        #endregion
+
+        #region ScreenSizeFilterTests
+
         [Fact]
-        public void ScreenSize()
+        public void WhenParticularScreenSizeIsRequiredByClientThenReturnNothingIfPassedProductsNotHaveSameScreenSize()
         {
             var check = new List<string>() { "abc1", "abc2", "abc3" };
             Assert.Equal(new List<string>() { "abc1", "abc2" },
                 _filter.FilterByScreenSize(check, "9"));
         }
         [Fact]
-        public void ScreenSize1()
+        public void WhenParticularScreenSizeIsRequiredByClientThenReturnProductsHavingSimilarScreenSize()
         {
             var check = new List<string>() { "abc3", "abc4" };
-            Assert.False(_filter.FilterByScreenSize(check, "9").Any());
+            Assert.Empty(_filter.FilterByScreenSize(check, "9"));
         }
         [Fact]
-        public void SizeScreenNull()
+        public void WhenEmptyInputListOfProductIdsIsPassedToScreenSizeFilterThenSendBackBadRequestStatusCode()
         {
             var check = new List<string>() { "" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByScreenSize(check, "true").ElementAt(0));
@@ -269,42 +289,49 @@ namespace ProductInfoApi.Tests
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByScreenSize(check, "true").ElementAt(0));
         }
 
+        #endregion
+
+        #region WeightFiterTests
 
         [Fact]
-        public void Weight()
+        public void WhenSpecificWeightIsRequiredByClientThenReturnProductsThatHaveSimilarWeight()
         {
             var checkBatteryProducts = new List<string>() { "abc1", "abc2", "abc3" };
             Assert.Equal(new List<string>() { "abc3" },
                 _filter.FilterByWeight(checkBatteryProducts, "9"));
         }
         [Fact]
-        public void Weight1()
+        public void WhenSpecificWeightIsRequiredByClientThenReturnNothingIfPassedProductsNotHaveSimilarWeight()
         {
             var checkBatteryProducts = new List<string>() { "abc1", "abc2" };
-            Assert.False(_filter.FilterByWeight(checkBatteryProducts, "9").Any());
+            Assert.Empty(_filter.FilterByWeight(checkBatteryProducts, "9"));
         }
         [Fact]
-        public void WeightNull()
+        public void WhenEmptyInputListOfProductIdsIsPassedToWeightFilterThenSendBackBadRequestStatusCode()
         {
             var checkWeight = new List<string>() { "" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByWeight(checkWeight, "9").ElementAt(0));
         }
         [Fact]
-        public void WeightDoubleCheck()
+        public void WeightIsDoubleCheck()
         {
             var checkWeight = new List<string>() { "abc1" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByWeight(checkWeight, "true").ElementAt(0));
         }
 
+        #endregion
+
+        #region ScreenType
+
         [Fact]
-        public void ScreenTypeNull()
+        public void WhenEmptyInputListOfProductIdsIsPassedToScreenTypeFilterThenSendBackBadRequestStatusCode()
         {
             var checkScreenType = new List<string>() { "" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByScreenType(checkScreenType, "9").ElementAt(0));
         }
 
         [Fact]
-        public void ScreenTypeCheck()
+        public void WhenWrongScreenTypeIsPassedThenReturnBadRequest()
         {
             var checkScreenType = new List<string>() { "abc1" };
             var output = _filter.FilterByScreenType(checkScreenType, "lued");
@@ -313,21 +340,25 @@ namespace ProductInfoApi.Tests
 
 
         [Fact]
-        public void ScreenType1()
+        public void WhenSpecificRequiredScreenTypeIsAvailableInPassedProductsThenReturnThoseProductsBack()
         {
             var checkScreenType = new List<string>() { "abc1", "abc2", "abc3", "abc4" };
             Assert.Equal(new List<string>() { "abc4" },
                 _filter.FilterByScreenType(checkScreenType, "lcd"));
         }
         [Fact]
-        public void ScreenType2()
+        public void WhenParticularScreenTypeIsNotAvailableInTheProvidedProductsThenReturnEmptyList()
         {
             var checkScreenType = new List<string>() { "abc3", "abc2" };
-            Assert.False(_filter.FilterByScreenType(checkScreenType, "Lcd").Any());
+            Assert.Empty(_filter.FilterByScreenType(checkScreenType, "Lcd"));
         }
 
+        #endregion
+
+        #region DimensionsFilterTests
+
         [Fact]
-        public void DimensionNull()
+        public void WhenEmptyInputListOfProductIdsIsPassedToDimensionFilterThenSendBackBadRequestStatusCode()
         {
             var inputList = new List<string>() { "" };
             Assert.Equal(HttpStatusCode.BadRequest, _filter.FilterByDimensions(
@@ -358,6 +389,11 @@ namespace ProductInfoApi.Tests
                 inputList, "6.1", "abc", "11.1").ElementAt(0));
         }
 
+
+        #endregion
+
+        #region GetSelectedProductDetailsTests
+
         [Fact]
         public void GetSelectedProductDetailsNoInput()
         {
@@ -372,16 +408,18 @@ namespace ProductInfoApi.Tests
             Assert.Equal("abc1", _filter.GetSelectedProductDetails(inputList).ElementAt(0).Key);
         }
 
+        #endregion
+
         #region HelperFunctionTests
 
         [Fact]
-        public void SubsetOfDictionaryPassingNull()
+        public void WhenSubsetOfDictionaryPassingNullThenReturnNull()
         {
             Assert.Null(HelperFunctions.SubsetOfDictionary(new List<object>() { "abc1" }, new List<KeyValuePair<string, ProductData>>()));
             Assert.Empty(HelperFunctions.SubsetOfDictionary(new List<object>() { "" }, _filter.DictionaryOfProducts));
         }
         [Fact]
-        public void SubsetOfDictionaryPassingValues()
+        public void WhenSubsetOfDictionaryPassingValuesThenReturnSubsetDictionaryWithSameProductIds()
         {
             Assert.NotNull(HelperFunctions.SubsetOfDictionary(new List<object>() { "abc1" }, _filter.DictionaryOfProducts));
             Assert.NotEmpty(HelperFunctions.SubsetOfDictionary(new List<object>() { "abc1" }, _filter.DictionaryOfProducts));
@@ -395,6 +433,25 @@ namespace ProductInfoApi.Tests
             Assert.False(HelperFunctions.IsInputEmptyOrNull(new List<string>() { "abc1" }));
 
         }
+        #endregion
+
+        #region ProductDataRemainingPropertyTests
+
+        [Fact]
+        public void ModelNumberInitializeTest()
+        {
+            var abc = new ProductData {ModelNumber = "1234Abc"};
+            Assert.Equal("1234Abc",abc.ModelNumber);
+
+        }
+        [Fact]
+        public void ProductNameInitializeTest()
+        {
+            var abc = new ProductData { ProductName = "Intellect" };
+            Assert.Equal("Intellect", abc.ProductName);
+
+        }
+
         #endregion
     }
 }
--- ProductInfoApi/Startup.cs
+++ ProductInfoApi/Startup.cs
@@ -10,28 +10,20 @@ namespace ProductInfoApi
     public class Startup
     {
         public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+        {Configuration = configuration;}
 
         // ReSharper disable once UnusedAutoPropertyAccessor.Local
         private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-            services.AddSingleton<ICharacteristicWiseFilter, CharacteristicWiseFilter>();
-        }
+        { services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1); 
+            services.AddSingleton<ICharacteristicWiseFilter, CharacteristicWiseFilter>();}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
             app.UseMvc();
         }
     }
